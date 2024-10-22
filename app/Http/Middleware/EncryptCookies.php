a397403db687e7b1f61eff7a04b3b0a243c85502
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-10-22 03:40:50 -0300

    Update EncryptCookies.php

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -9,9 +9,12 @@ class EncryptCookies extends Middleware
     /**
      * The names of the cookies that should not be encrypted.
      *
-     * @var array<int, string>
+     * Cookies listed here will be stored as plain text, allowing for easier access
+     * by third-party services or JavaScript-based client-side processing.
+     *
+     * @var array<int, string> $except
      */
-    protected $except = [
-        //
+    protected array $except = [
+        'plain_user_preferences',
     ];
 }
