00685395cb14be008a62166b6e4a611b8857a91a
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-10-22 03:31:05 -0300

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
@@ -12,10 +13,11 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    protected function redirectTo(Request $request): ?string
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
+        return null;
     }
 }
