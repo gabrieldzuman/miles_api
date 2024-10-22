b68a6da114572ee2f639a433d9f6cb192b3a2a16
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-10-22 03:58:02 -0300

    Update PreventRequestsDuringMaintenance.php

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -9,9 +9,23 @@ class PreventRequestsDuringMaintenance extends Middleware
     /**
      * The URIs that should be reachable while maintenance mode is enabled.
      *
+     * URIs added to this list will bypass the maintenance mode restrictions, 
+     * allowing access to critical routes.
+     *
      * @var array<int, string>
      */
-    protected $except = [
-        //
+    protected array $except = [
+        'status',   
     ];
+
+    /**
+     * Determine if the request has a URI that should be reachable while in maintenance mode.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function shouldPassThrough($request): bool
+    {
+        return in_array($request->path(), $this->except);
+    }
 }
