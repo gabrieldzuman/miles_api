98304562fffd319da949f7175795948e1080a8c0
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2025-06-03 11:06:58 -0300

    Update UpdateClientRequest.php

--- app/Http/Requests/Api/UpdateClientRequest.php
+++ app/Http/Requests/Api/UpdateClientRequest.php
@@ -7,24 +7,18 @@ use Illuminate\Foundation\Http\FormRequest;
 class UpdateClientRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
+     * Autoriza a requisição.
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
+     * Regras de validação para a requisição.
      */
-    public function rules()
+    public function rules(): array
     {
-        return [
-            //
-        ];
+        return [];
     }
 }
